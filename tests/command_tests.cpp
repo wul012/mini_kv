1eecef4b66c966a498ae2c0de93cada30464980c
wul <2494447558@qq.com>
2026-06-08 11:04:16 +0800

    v777 advance draft instruction preflight value-shape-slot

--- tests/command_tests.cpp
+++ tests/command_tests.cpp
@@ -851,7 +851,7 @@ int main() {
     assert_response_contains(result, "\"sourceDraftAuthoringReadinessReleaseVersion\":\"v760\"");
     assert_response_contains(result, "\"sourceDraftAuthoringReadinessPublishedStageCount\":25");
     assert_response_contains(result, "\"sourceDraftAuthoringReadinessChainComplete\":true");
-    assert_response_contains(result, "\"instructionPreflightReleaseVersion\":\"v776\"");
+    assert_response_contains(result, "\"instructionPreflightReleaseVersion\":\"v777\"");
     assert_response_contains(result, "\"instructionSlotCount\":25");
     assert_response_contains(result, "\"instructionGuardCount\":25");
     assert_response_contains(result, "\"instructionProfileCount\":25");
