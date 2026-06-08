dc164b55ce03b68329179ff00f6b38e2049fc09c
wul <2494447558@qq.com>
2026-06-08 08:23:47 +0800

    v747 advance draft authoring readiness statement-digest

--- src/shard_readiness.cpp
+++ src/shard_readiness.cpp
@@ -47,7 +47,7 @@ namespace minikv::shard_readiness {
 namespace {
 
 constexpr std::string_view contract_version = "shard-readiness.v1";
-constexpr std::string_view release_version = "v746";
+constexpr std::string_view release_version = "v747";
 
 std::string json_string(std::string_view value) {
     return runtime_evidence::json_string(value);
@@ -69,7 +69,7 @@ std::string format_command_catalog_json() {
 
 std::string evidence_digest() {
     return runtime_evidence::digest(
-        "mini-kv-shard-readiness-v746",
+        "mini-kv-shard-readiness-v747",
         {
             {std::string{contract_version}},
             {std::string{version}},
